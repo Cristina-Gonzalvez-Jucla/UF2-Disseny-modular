d007efd591982c460b1724586eae83911bc0729e
Cristina Gonzalvez Jucla <cristinagonzalvezjucla@outlook.com>
2021-11-25 13:07:36 +0100

    f

---
+++ Activitats_Classe/src/task_1.java
@@ -0,0 +1,8 @@
+public class task_1 {
+    public static void main(String[] args) {
+        greating();
+    }
+     public static void greating(){
+         System.out.println("Hello world");
+     }
+}
