d606c9691195ba12a05e4cd272cc67e9dd0c2c3f
Cristina Gonzalvez Jucla <cristinagonzalvezjucla@outlook.com>
2021-12-09 20:08:47 +0100

    Divisio Recursiva per Resta

--- Recursivitat/src/task_01.java
+++ Recursivitat/src/task_01.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 /*
  *Programar un algoritmo recursivo que permita
  *hacer la división por restas sucesivas.
  */
 public class task_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indica el divident");
+        int divident = 0;
+        divident = comprobarIntegro(scanner,"Incica el divident" );
 
+
+        System.out.println("Incica el divisor");
+        int divisor = 0;
+        divisor = comprobarIntegro(scanner,"Incica el divisor" );
+
+        System.out.printf("El resultat d'aquesta divisio es de: " + divisioRecursiva(divident, divisor));
+    }
+
+    public static int divisioRecursiva(int a, int b) {
+
+        if (a < b) {
+            return 0;
+        } else {
+            return divisioRecursiva(a - b, b)+1;
+        }
+    }
+    public static int comprobarIntegro(Scanner scan, String mensajeError) {
+        while (!scan.hasNextInt()) {
+            System.out.println("Dades erroneas ");
+            System.out.println(mensajeError);
+            scan.next();
+        }
+        return scan.nextInt();
+    }
 }
